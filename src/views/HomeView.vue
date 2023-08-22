d8763b49e7689a3996af0bbded4e344471ba168b
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-08-23 00:53:14 +0400

    feat:add tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+        <p class="text-red-200">wdefgtrmyutnb</p>
+    </div>
 </template>
